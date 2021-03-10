d24e9f96727a0d2d3c853d25f59f2039c52b715f
nurrahmiardani <nurrahmiardani1510@gmail.com>
2021-03-10 20:18:07 +0700

    Update coba.php

--- pert 1/coba.php
+++ pert 1/coba.php
@@ -38,19 +38,49 @@
             'usia' => '20'
         ],
         [
-            'nama' => 'mous',
-            'nim' => '192410121001',
-            'usia' => '51'
+            'nama' => 'hiiiiii',
+            'nim' => '19241010131',
+            'usia' => '21'
         ],
         [
-            'nama' => 'dudud',
-            'nim' => '192420101008',
+            'nama' => 'ududuud',
+            'nim' => '192410101008',
             'usia' => '21'
         ]
     ];
 
     $arr2 = ['NURE', '192410101053', '20']
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Data Mahasiswa</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Umur</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($arr as $mhs): ?>
+                <tr>
+                    <td><?= $mhs['nama'] ?></td>
+                    <td><?= $mhs['nim'] ?></td>
+                    <td><?= $mhs['usia'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
 
 </body>
 </html>
